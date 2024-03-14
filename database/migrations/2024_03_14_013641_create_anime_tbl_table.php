ce4500ffe0d1c90454c26a781a06d014d5849f2f
Rupert Caingal <johnrupertcaingal@gmail.com>
2024-03-14 10:52:54 +0800

    Fix(migration file): Change uiid to uuid for the id of anime_tbl table

--- database/migrations/2024_03_14_013641_create_anime_tbl_table.php
+++ database/migrations/2024_03_14_013641_create_anime_tbl_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('anime_tbl', function (Blueprint $table) {
-            $table->uiid('id')->primary();
+            $table->uuid('id')->primary();
             $table->string('title');
             $table->string('category')->nullable(true);
             $table->string('description');
